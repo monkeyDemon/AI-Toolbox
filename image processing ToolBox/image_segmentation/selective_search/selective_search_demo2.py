e76ed6485148cc228e9e22a6ef49e6676ea5ee6c
monkeyDemon <anshengmath@163.com>
2018-11-13 10:33:18 +0800

    add selective search demo2

---
+++ image processing ToolBox/image_segmentation/selective_search/selective_search_demo2.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 12 20:36:01 2018
+
+selective search 调用demo2
+读取一张图片，调用opencv实现的selectivesearch算法得到目标建议框并展示
+
+Selective Search paper:
+J.R.R. Uijlings, et al, Selective Search for Object Recognition, IJCV 2012
+
+@author: zyb_as
+"""
+
+import sys
+import cv2
+
+if __name__ == '__main__':
+
+    img_path = './cake.jpg'
+    mode = 'f'    # Switch to fast but low recall Selective Search method
+    # mode = 'q'   # Switch to high recall but slow Selective Search method
+    
+    
+    # speed-up using multithreads
+    cv2.setUseOptimized(True);
+    cv2.setNumThreads(4);
+
+    # read image
+    im = cv2.imread(img_path)
+    # resize image
+    newHeight = 200
+    newWidth = int(im.shape[1]*200/im.shape[0])
+    im = cv2.resize(im, (newWidth, newHeight))    
+
+    # create Selective Search Segmentation Object using default parameters
+    ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
+
+    # set input image on which we will run segmentation
+    ss.setBaseImage(im)
+
+    # Switch to fast but low recall Selective Search method
+    if (mode == 'f'):
+        ss.switchToSelectiveSearchFast()
+
+    # Switch to high recall but slow Selective Search method
+    elif (mode == 'q'):
+        ss.switchToSelectiveSearchQuality()
+    # if argument is neither f nor q print help message
+    else:
+        print(__doc__)
+        sys.exit(1)
+
+    # run selective search segmentation on input image
+    rects = ss.process()
+    print('Total Number of Region Proposals: {}'.format(len(rects)))
+
+    # number of region proposals to show
+    numShowRects = 100
+    # increment to increase/decrease total number
+    # of reason proposals to be shown
+    increment = 50
+
+    while True:
+        # create a copy of original image
+        imOut = im.copy()
+
+        # itereate over all the region proposals
+        for i, rect in enumerate(rects):
+            # draw rectangle for region proposal till numShowRects
+            if (i < numShowRects):
+                x, y, w, h = rect
+                cv2.rectangle(imOut, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
+            else:
+                break
+
+        # show output
+        cv2.imshow("Output", imOut)
+
+        # record key press
+        k = cv2.waitKey(0) & 0xFF
+
+        # m is pressed
+        if k == 109:
+            # increase total number of rectangles to show by increment
+            numShowRects += increment
+        # l is pressed
+        elif k == 108 and numShowRects > increment:
+            # decrease total number of rectangles to show by increment
+            numShowRects -= increment
+        # q is pressed
+        elif k == 113:
+            break
+    # close image show window
+    cv2.destroyAllWindows()
