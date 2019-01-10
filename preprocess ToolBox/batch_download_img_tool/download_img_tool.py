fdd90885dff8bd07266607640ea382535ea89b58
monkeyDemon <anshengmath@163.com>
2019-01-10 20:07:10 +0800

    modify preprocess tool: download_img_tool.py

--- preprocess ToolBox/batch_download_img_tool/download_img_tool.py
+++ preprocess ToolBox/batch_download_img_tool/download_img_tool.py
@@ -4,7 +4,7 @@ Created on Thu Oct 30 17:03:52 2018
 
 A image download file
 
-Read the text file which records url list of all pictures and download it in batches
+Read the text file which records url list of all pictures and download 
 
 In the work, in order to efficiently transfer a large amount of image data, 
 we usually only transmite a text file that records image information according 
@@ -13,47 +13,138 @@ to a certain structure.
 This tool shows how to download images by reading such a text file.
 The structure of the file needs to be similar to the 'url_record'
 
-@author: zyb
+python version: python 3.5
+
+@author: zyb_as
 """
+
 import os
+import sys
+import argparse, textwrap
 import requests
+import numpy as np
 
-# TODO: modify these two parameter when use
-download_path = "download/"        # download images save path
-urlfile = "url_record"            # url record txt file
+# set options
+parser = argparse.ArgumentParser(description = 'manual to this script', 
+        usage = textwrap.dedent('''\
+        command example:
+        python %(prog)s --file_type='npy' --file_path='test.npy' --save_dir='./download'
+        python %(prog)s --file_type='txt' --file_path='test.txt' --save_dir='./download' --splitter='\\t' --fields_num=1 --url_field_idx=0
+        use "python %(prog)s --help" for more information '''),
+        formatter_class = argparse.RawTextHelpFormatter)
+parser.add_argument('--file_type', type = str, default = None,
+        help = textwrap.dedent('''\
+        value: \'npy\' or \'txt\'
+        type of the file storing the url list.'''))
+parser.add_argument('--file_path', type = str, default = None,
+        help = 'the path of the file storing the url list')
+parser.add_argument('--save_dir', type = str, default = None,
+        help = 'the directory to save the download images')
+parser.add_argument('--splitter', type = str, default = '\t',
+        help = textwrap.dedent('''\
+        use when file_type=\'txt\'
+        the splitter for each line in the txt file.'''))
+parser.add_argument('--fields_num', type = int, default = 1,
+        help = textwrap.dedent('''\
+        use when file_type=\'txt\'
+        the fields number each line.'''))
+parser.add_argument('--url_field_idx', type = int, default = 0,
+        help = textwrap.dedent('''\
+        use when file_type=\'txt\'
+        the idx of the url field in each line.'''))
 
 
-def get_img_by_url(url):
+
+def _get_img_by_url(url, save_dir):
     """
-    download and write a image by url
-    url example: http://img.zuoyebang.cc/zyb_2142d51950cdb170b7729320eca2a545.jpg
+    download and save a image by url
     """
+    if save_dir[-1] != '/': # make sure save_dir end with '/'
+        save_dir += '/'
+    
+    # request url
     #response = requests.get(url,timeout=0.1,proxies=proxies)
     pic_name = url.split('/')[-1]
     response = requests.get(url,timeout=1)
-    #print(pic_name,'  OK')
-    with open(download_path + pic_name, 'wb') as fd:
+    print(pic_name,'  OK')
+    
+    # save image
+    with open(save_dir + pic_name, 'wb') as fd:
         for chunk in response.iter_content(128):  # iter_content saving while downloading
             fd.write(chunk)    
 
 
-if os.path.exists(download_path) == False:
-    os.mkdir(download_path)
-failure_count = 0
-failed_urls = []
-cnt = 0
-for line in open(urlfile).readlines():
-    cnt += 1
-
-    item = line.split('\t')
-    url = item[-1]  # TODO: these two line may be need to modify(depend on the structure of txt file)
-    if len(item) >= 3:  # TODO: ditto
+def download_from_txt(url_file, save_dir, splitter = '\t', fields_num = 1, url_field_idx = 0):    
+    if os.path.exists(save_dir):
+        print("warning! save_dir has exists, please check!")
+    else:
+        os.mkdir(save_dir)
+    
+    if splitter == '\\t':
+        splitter = '\t'
+    
+    failure_count = 0
+    failed_urls = []
+    cnt = 0                 #spammer_urls
+    for line in open(url_file).readlines():
+        cnt += 1   
+        item = line.split(splitter)
+        if len(item) == fields_num: 
+            try:
+                url = item[url_field_idx]
+                url = 'http://img.zuoyebang.cc/' + url.rstrip('\n')
+                # rstrip() delete the specified character at the end of the string (default is a space)
+                print(url)
+                _get_img_by_url(url, save_dir)  
+                break
+            except:
+                failure_count += 1
+                failed_urls.append(url.rstrip())
+    print("\nfailed record:")
+    print(failure_count)
+    print(failed_urls)
+    
+    # retry the download failed urls
+    print("\nretry the download failed urls...")
+    for url in failed_urls:
         try:
             print(url)
-            get_img_by_url(url.rstrip())  # rstrip() delete the specified character at the end of the string string (default is a space)
+            _get_img_by_url(url, save_dir)  
         except:
             failure_count += 1
-            failed_urls.append(url.rstrip())
+            failed_urls.append(url)
+
+
+
+def download_from_npy(npy_file, save_dir):
+    if len(save_dir) == 0:
+        raise("error! save_dir length is 0")
+    if os.path.exists(save_dir):
+        print("warning! save_dir has exists, please check!")
+    else:
+        os.mkdir(save_dir)
     
-print(failure_count)
-print(failed_urls)
+    url_list = np.load(npy_file)
+    for url in url_list:
+        #url = url.decode('utf8')
+        _get_img_by_url(url, save_dir)
+
+
+
+if __name__ == "__main__":
+    # get options
+    args = parser.parse_args()
+    file_type = args.file_type
+    file_path = args.file_path
+    save_dir = args.save_dir
+    
+    if file_type == 'npy':
+        download_from_npy(file_path, save_dir)
+    elif file_type == 'txt':
+        splitter = args.splitter
+        fields_num = args.fields_num
+        url_field_idx = args.url_field_idx
+        download_from_txt(file_path, save_dir, splitter, fields_num, url_field_idx)
+    else:
+        print('Error use of --file_type! Please use -h to check')
+        sys.exit()
