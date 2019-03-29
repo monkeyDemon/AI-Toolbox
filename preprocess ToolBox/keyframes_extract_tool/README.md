1405fa38b38efb154980c5ebd1195b364ca1e34a
He Gong <he-gong@qq.com>
2019-03-29 01:44:27 -0400

    save memory by closing plt. Make the function a class and easy to use. Add logging. rewrite Frame class.

---
+++ preprocess ToolBox/keyframes_extract_tool/README.md
@@ -0,0 +1,6 @@
+# Usage example:
+from keyframes_extract_diff import KeyFrames
+keyframes = KeyFrames() 
+# keyframes = KeyFrames(mode=KeyFrames.USE_LOCAL_MAXIMA | KeyFrames.USE_THRESH | KeyFrames.USE_TOP_ORDER, debug = True, thresh=0.7, len_window=60 )
+keyframes.extract("pikachu.mp4") 
+# keyframes.extract("pikachu.mp4", mode=KeyFrames.USE_THRESH, debug = False)
--- preprocess ToolBox/keyframes_extract_tool/keyframes_extract_diff.py
+++ preprocess ToolBox/keyframes_extract_tool/keyframes_extract_diff.py
@@ -37,10 +37,14 @@ import cv2
 import operator
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 from scipy.signal import argrelextrema
+import gc
+import psutil, os, sys
+from time import time
+import logging
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
- 
 def smooth(x, window_len=13, window='hanning'):
     """smooth the data using a window with requested size.
     
@@ -70,7 +74,7 @@ def smooth(x, window_len=13, window='hanning'):
  
     TODO: the window parameter could be the window itself if an array instead of a string   
     """
-    print(len(x), window_len)
+    logger.info("length of frames: %d" % len(x))
     # if x.ndim != 1:
     #     raise ValueError, "smooth only accepts 1 dimension arrays."
     #
@@ -93,120 +97,136 @@ def smooth(x, window_len=13, window='hanning'):
         w = getattr(np, window)(window_len)
     y = np.convolve(w / w.sum(), s, mode='same')
     return y[window_len - 1:-window_len + 1]
- 
 
-class Frame:
-    """class to hold information about each frame
-    
-    """
+class Frame():
     def __init__(self, id, diff):
-        self.id = id
-        self.diff = diff
- 
-    def __lt__(self, other):
-        if self.id == other.id:
-            return self.id < other.id
-        return self.id < other.id
- 
-    def __gt__(self, other):
-        return other.__lt__(self)
- 
-    def __eq__(self, other):
-        return self.id == other.id and self.id == other.id
- 
-    def __ne__(self, other):
-        return not self.__eq__(other)
- 
- 
+        self.id, self.diff = id, diff
+
 def rel_change(a, b):
    x = (b - a) / max(a, b)
-   print(x)
+   logger.debug(x)
    return x
- 
-    
-if __name__ == "__main__":
-    print(sys.executable)
-    #Setting fixed threshold criteria
-    USE_THRESH = False
-    #fixed threshold value
-    THRESH = 0.6
-    #Setting fixed threshold criteria
-    USE_TOP_ORDER = False
-    #Setting local maxima criteria
-    USE_LOCAL_MAXIMA = True
-    #Number of top sorted frames
-    NUM_TOP_FRAMES = 50
-     
-    #Video path of the source file
-    videopath = 'pikachu.mp4'
-    #Directory to store the processed frames
-    dir = './extract_result/'
-    #smoothing window size
-    len_window = int(50)
-    
-    
-    print("target video :" + videopath)
-    print("frame save directory: " + dir)
-    # load video and compute diff between frames
-    cap = cv2.VideoCapture(str(videopath)) 
-    curr_frame = None
-    prev_frame = None 
-    frame_diffs = []
-    frames = []
-    success, frame = cap.read()
-    i = 0 
-    while(success):
-        luv = cv2.cvtColor(frame, cv2.COLOR_BGR2LUV)
-        curr_frame = luv
-        if curr_frame is not None and prev_frame is not None:
-            #logic here
-            diff = cv2.absdiff(curr_frame, prev_frame)
-            diff_sum = np.sum(diff)
-            diff_sum_mean = diff_sum / (diff.shape[0] * diff.shape[1])
-            frame_diffs.append(diff_sum_mean)
-            frame = Frame(i, diff_sum_mean)
-            frames.append(frame)
-        prev_frame = curr_frame
-        i = i + 1
-        success, frame = cap.read()   
-    cap.release()
-    
-    # compute keyframe
-    keyframe_id_set = set()
-    if USE_TOP_ORDER:
-        # sort the list in descending order
-        frames.sort(key=operator.attrgetter("diff"), reverse=True)
-        for keyframe in frames[:NUM_TOP_FRAMES]:
-            keyframe_id_set.add(keyframe.id) 
-    if USE_THRESH:
-        print("Using Threshold")
-        for i in range(1, len(frames)):
-            if (rel_change(np.float(frames[i - 1].diff), np.float(frames[i].diff)) >= THRESH):
-                keyframe_id_set.add(frames[i].id)   
-    if USE_LOCAL_MAXIMA:
-        print("Using Local Maxima")
-        diff_array = np.array(frame_diffs)
-        sm_diff_array = smooth(diff_array, len_window)
-        frame_indexes = np.asarray(argrelextrema(sm_diff_array, np.greater))[0]
-        for i in frame_indexes:
-            keyframe_id_set.add(frames[i - 1].id)
-            
-        plt.figure(figsize=(40, 20))
-        plt.locator_params(numticks=100)
-        plt.stem(sm_diff_array)
-        plt.savefig(dir + 'plot.png')
-    
-    # save all keyframes as image
-    cap = cv2.VideoCapture(str(videopath))
-    curr_frame = None
-    keyframes = []
-    success, frame = cap.read()
-    idx = 0
-    while(success):
-        if idx in keyframe_id_set:
-            name = "keyframe_" + str(idx) + ".jpg"
-            cv2.imwrite(dir + name, frame)
-            keyframe_id_set.remove(idx)
-        idx = idx + 1
+
+class KeyFrames():
+    USE_TOP_ORDER = 1  # Setting fixed threshold criteria
+    USE_THRESH = 2
+    USE_LOCAL_MAXIMA = 4
+    def __init__(self, mode = 4, debug = False, **kwargs):
+        #Constant
+
+        self.set(mode, debug, **kwargs)
+
+    def set(self, mode, debug = -1, **kwargs):
+        if mode:
+            self.use_top_order = 1 & mode
+            self.use_thresh = 2 & mode
+            self.use_local_maxima = 4 & mode
+            if self.use_thresh:
+                logger.info("Use Threshold")
+                self.thresh = kwargs.get("thresh", 0.6)
+            if self.use_top_order:
+                logger.info("Use top order")
+                self.num_top_frames = kwargs("num_top_frames", 50)
+            if self.use_local_maxima:
+                self.len_window = kwargs.get("len_window", 50)
+                logger.info("Use Local Maxima")
+        if debug == -1:
+            pass
+        elif debug:
+            logger.setLevel(logging.DEBUG)
+        else:
+            logger.setLevel(logging.INFO)
+
+    #@video_path : Video path of the source file
+    #@dir : Directory to store the processed frames
+    def extract(self, video_path, dir=None, mode=0, debug = -1, **kwargs):
+        self.set(mode, debug, **kwargs)
+
+        start_time = time()
+        logger.debug(sys.executable)
+        if not dir:
+            dir = os.path.splitext(video_path)[0]
+            if not os.path.exists(dir):
+                os.mkdir(dir)
+
+        logger.debug("initial allocated memory: %d MB" % (psutil.Process(os.getpid()).memory_info().rss >> 20))
+        logger.info("target video: " + os.path.abspath(video_path))
+        logger.info("frame save directory: " + os.path.abspath(dir))
+
+        # load video and compute diff between frames
+        cap = cv2.VideoCapture(str(video_path))
+        curr_frame = None
+        prev_frame = None
+        frame_diffs = []
+        frames = []
         success, frame = cap.read()
-    cap.release()
+        i = 0
+        while(success):
+            luv = cv2.cvtColor(frame, cv2.COLOR_BGR2LUV)
+            curr_frame = luv
+            if curr_frame is not None and prev_frame is not None:
+                #logic here
+                diff = cv2.absdiff(curr_frame, prev_frame)
+                diff_sum = np.sum(diff)
+                diff_sum_mean = diff_sum / (diff.shape[0] * diff.shape[1])
+                frame_diffs.append(diff_sum_mean)
+                frame = Frame(i, diff_sum_mean)
+                frames.append(frame)
+            prev_frame = curr_frame
+            i = i + 1
+            success, frame = cap.read()
+        cap.release()
+
+        # compute keyframe
+        keyframe_id_set = set()
+        if self.use_top_order:
+            # sort the list in descending order
+            frames.sort(key=operator.attrgetter("diff"), reverse=True)
+            for keyframe in frames[:self.num_top_frames]:
+                keyframe_id_set.add(keyframe.id)
+        if self.use_thresh:
+            for i in range(1, len(frames)):
+                if (rel_change(np.float(frames[i - 1].diff), np.float(frames[i].diff)) >= self.thresh):
+                    keyframe_id_set.add(frames[i].id)
+        if self.use_local_maxima:
+            diff_array = np.array(frame_diffs)
+            sm_diff_array = smooth(diff_array, self.len_window)
+            frame_indexes = np.asarray(argrelextrema(sm_diff_array, np.greater))[0]
+            for i in frame_indexes:
+                keyframe_id_set.add(frames[i - 1].id)
+
+            plt.figure(figsize=(40, 20))
+            plt.locator_params(numticks=100)
+            plt.stem(sm_diff_array)
+            plt.savefig(os.path.join(dir, 'plot.png'))
+            plt.close('all'); gc.collect(); # these 2 statements will release the memory of plt and save 60MB
+
+        # save all keyframes as image
+        cap = cv2.VideoCapture(str(video_path))
+        curr_frame = None
+        keyframes = []
+        success, frame = cap.read()
+        idx = 0
+
+        while(success):
+            if idx in keyframe_id_set:
+                name = "keyframe_%04d.jpg" %idx
+                cv2.imwrite(os.path.join(dir, name), frame)
+                keyframe_id_set.remove(idx)
+            idx = idx + 1
+            success, frame = cap.read()
+
+        cap.release()
+
+        end_time = time()
+        logger.info("elapsed time: %d" % (end_time - start_time))
+        logger.debug("end allocated memory: %dMB" % (psutil.Process(os.getpid()).memory_info().rss >> 20))
+
+
+if __name__ == "__main__":
+    # Usage example:
+    # from keyframes_extract_diff import KeyFrames
+    keyframes = KeyFrames() # this is the easiest way to use
+    # keyframes = KeyFrames(mode=6)  # this works
+    # keyframes = KeyFrames(mode=KeyFrames.USE_LOCAL_MAXIMA | KeyFrames.USE_THRESH, debug = True, thresh=0.7, len_window=60 ) # this also works
+    keyframes.extract("pikachu.mp4")
