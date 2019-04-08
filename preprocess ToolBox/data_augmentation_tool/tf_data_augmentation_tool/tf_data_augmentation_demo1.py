216df1585a211a9c84c10853b1b62fac2185dc07
monkeyDemon <anshengmath@163.com>
2019-04-08 22:44:15 +0800

    add tf data augmentation quick start demo

---
+++ preprocess ToolBox/data_augmentation_tool/tf_data_augmentation_tool/tf_data_augmentation_demo1.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 21:30:49 2019
+
+demo to use tensorflow api do data augmentation
+
+this is a quick start demo
+just test the basic tensorflow api and show
+
+@author: zyb_as
+"""
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+image_raw_data = tf.gfile.FastGFile('./data_augmentation_test/1.jpg','rb').read()
+
+with tf.Session() as sess:
+    img_data = tf.image.decode_jpeg(image_raw_data)
+    plt.imshow(img_data.eval())
+    plt.show()
+
+    # -----color space transformation-----
+    # 将图片的亮度-0.5。
+    adjusted = tf.image.adjust_brightness(img_data, -0.5)
+    plt.imshow(adjusted.eval())
+    plt.show()
+
+    # 将图片的亮度0.5
+    adjusted = tf.image.adjust_brightness(img_data, 0.5)
+    plt.imshow(adjusted.eval())
+    plt.show()
+    # 在[-max_delta, max_delta)的范围随机调整图片的亮度。
+    adjusted = tf.image.random_brightness(img_data, max_delta=0.5)
+    plt.imshow(adjusted.eval())
+    plt.show()
+     
+    # 将图片的对比度-5
+    adjusted = tf.image.adjust_contrast(img_data, -5)
+    plt.imshow(adjusted.eval())
+    plt.show()
+    # 将图片的对比度+5
+    adjusted = tf.image.adjust_contrast(img_data, 5)
+    plt.imshow(adjusted.eval())
+    plt.show()
+    # 在[lower, upper]的范围随机调整图的对比度。
+    adjusted = tf.image.random_contrast(img_data, 0.1, 0.6)
+    plt.imshow(adjusted.eval())
+    plt.show()
+     
+    #调整图片的色相
+    adjusted = tf.image.adjust_hue(img_data, 0.1)
+    plt.imshow(adjusted.eval())
+    plt.show()
+     
+    # 在[-max_delta, max_delta]的范围随机调整图片的色相。max_delta的取值在[0, 0.5]之间。
+    adjusted = tf.image.random_hue(img_data, 0.5)
+    plt.imshow(adjusted.eval())
+    plt.show()  
+
+
+    # 将图片的饱和度-5。
+    adjusted = tf.image.adjust_saturation(img_data, -5)
+    plt.imshow(adjusted.eval())
+    plt.show()  
+
+
+    # 在[lower, upper]的范围随机调整图的饱和度。
+    adjusted = tf.image.random_saturation(img_data, 0, 5)
+    plt.imshow(adjusted.eval())
+    plt.show()    
+    
+    # -----position transformation-----
+    
+    # 上下翻转
+    flipped1 = tf.image.flip_up_down(img_data)
+    plt.imshow(flipped1.eval())
+    plt.show()
+    # 左右翻转
+    flipped2 = tf.image.flip_left_right(img_data)
+    plt.imshow(flipped2.eval())
+    plt.show()
+    #对角线翻转
+    transposed = tf.image.transpose_image(img_data)
+    plt.imshow(transposed.eval())
+    plt.show()
+
+    # -----normalization-----
+    
+    # 将代表一张图片的三维矩阵中的数字均值变为0，方差变为1。
+    adjusted = tf.image.per_image_standardization(img_data)
