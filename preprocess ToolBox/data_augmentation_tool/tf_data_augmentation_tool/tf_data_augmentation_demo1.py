ff37eb49566ac6870737d3416319032e1f3baf25
monkeyDemon <anshengmath@163.com>
2019-04-10 17:55:19 +0800

    add tf data augmentation demo1: quick start

--- preprocess ToolBox/data_augmentation_tool/tf_data_augmentation_tool/tf_data_augmentation_demo1.py
+++ preprocess ToolBox/data_augmentation_tool/tf_data_augmentation_tool/tf_data_augmentation_demo1.py
@@ -16,56 +16,69 @@ import tensorflow as tf
 image_raw_data = tf.gfile.FastGFile('./data_augmentation_test/1.jpg','rb').read()
 
 with tf.Session() as sess:
+    print("\n original image")
     img_data = tf.image.decode_jpeg(image_raw_data)
     plt.imshow(img_data.eval())
     plt.show()
 
     # -----color space transformation-----
     # 将图片的亮度-0.5。
+    print("\n brightness - 0.5")
     adjusted = tf.image.adjust_brightness(img_data, -0.5)
     plt.imshow(adjusted.eval())
     plt.show()
 
-    # 将图片的亮度0.5
+    # 将图片的亮度+0.5
+    print("\n brightness + 0.5")
     adjusted = tf.image.adjust_brightness(img_data, 0.5)
     plt.imshow(adjusted.eval())
     plt.show()
+    
     # 在[-max_delta, max_delta)的范围随机调整图片的亮度。
+    print("\n random adjust brightness [-0.5,0.5]")
     adjusted = tf.image.random_brightness(img_data, max_delta=0.5)
     plt.imshow(adjusted.eval())
     plt.show()
      
     # 将图片的对比度-5
+    print("\n adjust contrast -5")
     adjusted = tf.image.adjust_contrast(img_data, -5)
     plt.imshow(adjusted.eval())
     plt.show()
+    
     # 将图片的对比度+5
+    print("\n adjust contrast +5")
     adjusted = tf.image.adjust_contrast(img_data, 5)
     plt.imshow(adjusted.eval())
     plt.show()
+    
     # 在[lower, upper]的范围随机调整图的对比度。
+    print("\n random adjust contrast [0.1,0.6]")
     adjusted = tf.image.random_contrast(img_data, 0.1, 0.6)
     plt.imshow(adjusted.eval())
     plt.show()
      
     #调整图片的色相
+    print("\n adjust hue 0.1")
     adjusted = tf.image.adjust_hue(img_data, 0.1)
     plt.imshow(adjusted.eval())
     plt.show()
      
     # 在[-max_delta, max_delta]的范围随机调整图片的色相。max_delta的取值在[0, 0.5]之间。
+    print("\n random adjust hue [0, 0.5]")
     adjusted = tf.image.random_hue(img_data, 0.5)
     plt.imshow(adjusted.eval())
     plt.show()  
 
 
     # 将图片的饱和度-5。
+    print("\n adjust saturation -5")
     adjusted = tf.image.adjust_saturation(img_data, -5)
     plt.imshow(adjusted.eval())
     plt.show()  
 
-
     # 在[lower, upper]的范围随机调整图的饱和度。
+    print("\n random adjust saturation [0,5]")
     adjusted = tf.image.random_saturation(img_data, 0, 5)
     plt.imshow(adjusted.eval())
     plt.show()    
@@ -73,14 +86,19 @@ with tf.Session() as sess:
     # -----position transformation-----
     
     # 上下翻转
+    print("\n filp up down")
     flipped1 = tf.image.flip_up_down(img_data)
     plt.imshow(flipped1.eval())
     plt.show()
+    
     # 左右翻转
+    print("\n filp left right")
     flipped2 = tf.image.flip_left_right(img_data)
     plt.imshow(flipped2.eval())
     plt.show()
+    
     #对角线翻转
+    print("\n transpose image")
     transposed = tf.image.transpose_image(img_data)
     plt.imshow(transposed.eval())
     plt.show()
