932b783601447fcd720c0fc4471bbb7159c5c4c0
monkeyDemon <anshengmath@163.com>
2019-04-30 17:17:01 +0800

    add script to get tags list

--- preprocess ToolBox/web_crawler_demo/danbooru_crawler/get_tag_list.py
+++ preprocess ToolBox/web_crawler_demo/danbooru_crawler/get_tag_list.py
@@ -12,19 +12,22 @@ import xml.dom.minidom
 
 
 all_tag_path = 'tmp/all_tags.xml'
-most_frequency_num = 100
+frequency_tag_save_path = 'tmp/high_frequency_tags_list.txt'
+most_frequency_num = 2000
+tags_of_interest = set(["0", "3", "4"])
 
 # 使用minidom解析器打开 XML 文档
 DOMTree = xml.dom.minidom.parse(all_tag_path)
 collection = DOMTree.documentElement
-
 
 tags = collection.getElementsByTagName("tag")
 
 cnt_list = []
 for tag in tags:
     tag_cnt = int(tag.getAttribute('count'))
-    cnt_list.append(tag_cnt)
+    tag_type = tag.getAttribute('type')
+    if tag_type in tags_of_interest:
+        cnt_list.append(tag_cnt)
     
 cnt_list.sort(reverse=True)
 frequency_threshold = cnt_list[most_frequency_num]
@@ -32,10 +35,14 @@ print("highest frequency tag count: {}".format(cnt_list[0]))
 print("frequency threshold: {}".format(frequency_threshold))
 print("----------------------------------")
 
-for tag in tags:
-    tag_cnt = int(tag.getAttribute('count'))
-    if tag_cnt > frequency_threshold:
+with open(frequency_tag_save_path, 'w') as writer:
+    for tag in tags:
+        tag_cnt = int(tag.getAttribute('count'))
         tag_type = tag.getAttribute('type')
-        tag_name = tag.getAttribute('name')
-        #print("name: {}, type: {}".format(tag_name, tag_type))
-        print(tag_name)
+        if tag_cnt > frequency_threshold and tag_type in tags_of_interest:
+            tag_name = tag.getAttribute('name')
+            line = tag_type + '\t' + tag_name + '\t' + str(tag_cnt) + '\n'
+            writer.write(line)
+            #print("name: {}, type: {}".format(tag_name, tag_type))
+            #print(tag_name)
+print("finish")
