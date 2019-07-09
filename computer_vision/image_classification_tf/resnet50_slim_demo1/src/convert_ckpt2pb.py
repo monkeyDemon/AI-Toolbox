7e7a2e16d5cc42facaa6e9827e44d47643960c54
monkeyDemon <anshengmath@163.com>
2019-07-09 14:42:41 +0800

    :hammer: modify convert_ckpt2pb.py

--- computer_vision/image_classification_tf/resnet50_slim_demo1/src/convert_ckpt2pb.py
+++ computer_vision/image_classification_tf/resnet50_slim_demo1/src/convert_ckpt2pb.py
@@ -13,43 +13,19 @@ convert ckpt to pb
 
 @author: zyb_as
 """
-
+import os
 import tensorflow as tf
 from tensorflow.python.framework import graph_util
- 
 
-'''
-def freeze_graph_test(pb_path, image_path):
-    with tf.Graph().as_default():
-        output_graph_def = tf.GraphDef()
-        with open(pb_path, "rb") as f:
-            output_graph_def.ParseFromString(f.read())
-            tf.import_graph_def(output_graph_def, name="")
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
- 
-            # 定义输入的张量名称,对应网络结构的输入张量
-            # input:0作为输入图像,keep_prob:0作为dropout的参数,测试时值为1,is_training:0训练参数
-            input_image_tensor = sess.graph.get_tensor_by_name("input:0")
-            input_keep_prob_tensor = sess.graph.get_tensor_by_name("keep_prob:0")
-            input_is_training_tensor = sess.graph.get_tensor_by_name("is_training:0")
- 
-            # 定义输出的张量名称
-            output_tensor_name = sess.graph.get_tensor_by_name("InceptionV3/Logits/SpatialSqueeze:0")
- 
-            # 读取测试图片
-            im=read_image(image_path,resize_height,resize_width,normalization=True)
-            im=im[np.newaxis,:]
-            # 测试读出来的模型是否正确，注意这里传入的是输出和输入节点的tensor的名字，不是操作节点的名字
-            # out=sess.run("InceptionV3/Logits/SpatialSqueeze:0", feed_dict={'input:0': im,'keep_prob:0':1.0,'is_training:0':False})
-            out=sess.run(output_tensor_name, feed_dict={input_image_tensor: im,
-                                                        input_keep_prob_tensor:1.0,
-                                                        input_is_training_tensor:False})
-            print("out:{}".format(out))
-            score = tf.nn.softmax(out, name='pre')
-            class_id = tf.argmax(score, 1)
-            print("pre class_id:{}".format(sess.run(class_id)))
- '''
+flags = tf.app.flags
+flags.DEFINE_string('action', None, 'command action: print or convert')
+flags.DEFINE_string('checkpoint_path', None,
+                    'Path to trained checkpoint, typically of the form '
+                    'path/to/model.ckpt')
+flags.DEFINE_string('pb_model_save_path', None, 'Path to write pb model outputs')
+flags.DEFINE_string('cuda_visible_devices', '0', 'Specify which gpu to be used.')
+FLAGS = flags.FLAGS
+
 
 
 def freeze_graph(input_checkpoint, output_graph_path):
@@ -59,16 +35,17 @@ def freeze_graph(input_checkpoint, output_graph_path):
     :return:
     '''
  
-    # 指定输出的节点名称,该节点名称必须是原模型中存在的节点
     # TODO: modify output op name
-    output_node_names = "InceptionV3/Logits/SpatialSqueeze"
+    # 指定输出的节点名称,该节点名称必须是原模型中存在的节点
+    # use op name not tensor name(use "Logits/SpatialSqueeze" not "Logits/SpatialSqueeze:0")
+    output_node_names = "score_list"
     saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=True)
  
     with tf.Session() as sess:
         saver.restore(sess, input_checkpoint) #恢复图并得到数据
         output_graph_def = graph_util.convert_variables_to_constants(  # 模型持久化，将变量值固定
             sess=sess,
-            input_graph_def=sess.graph_def,# 等于:sess.graph_def
+            input_graph_def=sess.graph_def,
             output_node_names=output_node_names.split(","))# 如果有多个输出节点，以逗号隔开
  
         with tf.gfile.GFile(output_graph_path, "wb") as f: #保存模型
@@ -96,3 +73,20 @@ def print_op_names(checkpoint_path):
             for op in op_list:
                 print(op.name)
                 print(op.values())
+
+
+
+def main(_):
+    # Specify which gpu to be used
+    os.environ["CUDA_VISIBLE_DEVICES"] = FLAGS.cuda_visible_devices
+
+    if FLAGS.action == 'print':
+        print_op_names(FLAGS.checkpoint_path)
+    elif FLAGS.action == 'convert':
+        freeze_graph(FLAGS.checkpoint_path, FLAGS.pb_model_save_path)
+    else:
+        raise RuntimeError("parameter action error! use print or convert")
+
+
+if __name__ == '__main__':
+    tf.app.run()
