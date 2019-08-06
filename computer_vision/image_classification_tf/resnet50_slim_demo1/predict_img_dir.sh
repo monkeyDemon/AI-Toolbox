e0fccdf36968c1abee3e2f68efafd6e4eed4b4ee
monkeyDemon <anshengmath@163.com>
2019-08-06 10:57:50 +0800

    modify predict_img_dir.sh

--- computer_vision/image_classification_tf/resnet50_slim_demo1/predict_img_dir.sh
+++ computer_vision/image_classification_tf/resnet50_slim_demo1/predict_img_dir.sh
@@ -4,10 +4,11 @@
 echo "start predicting by src/predict.py ..."
 
 # model path can be both .pb or .ckpt or directory of savemodel
+weight_format='ckpt'  # ckpt, pb or savemodel
 model_path='log/train_tf_log/trained model ckpt path'
 #model_path='log/train_tf_log/frozen/frozen inference graph pb file' 
 #model_path='log/train_tf_log/frozen/directory of savemodel pb' 
-weight_format='ckpt'  # ckpt, pb or savemodel
+
 test_img_dir='dataset/testset'
 result_dir='test_predict'
 devices='0'
