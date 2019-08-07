bcda8d8809e13bed551130b5bec5774d296c8790
monkeyDemon <anshengmath@163.com>
2019-08-07 10:46:00 +0800

    :hammer: modify annotation information

--- computer_vision/image_classification_tf/resnet50_slim_demo1/src/train.py
+++ computer_vision/image_classification_tf/resnet50_slim_demo1/src/train.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-from __future__ import division
 """
 Created on Thu Oct 11 17:21:35 2018
 
-Train a CNN classification model via pretrained ResNet-50 model.
+Train a CNN classification model.
 
 @author: shirhe-lyh
 @modified: zyb_as
 """
+from __future__ import division
 import os
 import sys
 import math
@@ -15,39 +15,35 @@ import time
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-from tensorflow.contrib.slim import nets
-from tensorflow.python.ops import control_flow_ops
 
-import resnet_v1_50_model
 import data_provider
+
+# TODO: import the model you want to convert
+from net.resnet import resnet_v1_50_model as model
+#from net.resnet import resnet_v1_20_model as model
 
 
 slim = tf.contrib.slim
 flags = tf.app.flags
-
-# TODO: Modify parameter defaults here or specify them directly at call time
 flags.DEFINE_string('tf_record_dir', 
-                    '/home/ansheng/cv_strategy/porn_detect/cnn_tf/' +
-                    'resnet50_slim/tfrecord',
+                    'tfrecord',
                     'Directory to tfrecord files.')
 flags.DEFINE_string('checkpoint_path', 
-                    '/home/ansheng/cv_strategy/model_zoo/' +
-                    'resnet_v1_50.ckpt', 
+                    'model_zoo/resnet_v1_50.ckpt', 
                     'Path to pretrained ResNet-50 model.')
 flags.DEFINE_boolean('train_from_scratch', True, 
     'train from scratch on imagenet pretrained model or continue training on previous model')
 flags.DEFINE_string('label_path',
-                    '/home/ansheng/cv_strategy/porn_detect/cnn_tf/' +
-                    'classification_by_slim/tfrecord/labels.txt',
+                    'tfrecord/labels.txt',
                     'Path to label file.')
-flags.DEFINE_string('log_dir', './log/train_log', 'Path to log directory.')
+flags.DEFINE_string('log_dir', './log/train_tf_log', 'Path to log directory.')
 flags.DEFINE_string('gpu_device', '0', 'Specify which gpu to be used')
 flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
 flags.DEFINE_float(
-    'learning_rate_decay_factor', 0.1, 'Learning rate decay factor.')  # TODO: not use
+    'learning_rate_decay_factor', 0.1, 'Learning rate decay factor.')
 flags.DEFINE_integer(
     'num_epochs_per_decay', 3,
-    'Number of epochs after which learning rate decays. Note: this flag counts '  # TODO: not use
+    'Number of epochs after which learning rate decays. Note: this flag counts '
     'epochs per clone but aggregates per sync replicas. So 1.0 means that '
     'each clone will go over full epoch individually, but replicas will go '
     'once across all replicas.')
@@ -56,73 +52,6 @@ flags.DEFINE_integer('epoch_num', 10, 'Number of epochs.')
 flags.DEFINE_integer('batch_size', 48, 'Batch size')
 
 FLAGS = flags.FLAGS
-
-    
-'''    
-def configure_learning_rate(num_samples_per_epoch, global_step):
-    """Configures the learning rate.
-    
-    Modified from:
-        https://github.com/tensorflow/models/blob/master/research/slim/
-        train_image_classifier.py
-    
-    Args:
-        num_samples_per_epoch: he number of samples in each epoch of training.
-        global_step: The global_step tensor.
-        
-    Returns:
-        A `Tensor` representing the learning rate.
-    """
-    decay_steps = int(num_samples_per_epoch * FLAGS.num_epochs_per_decay /
-                      FLAGS.batch_size)
-    return tf.train.exponential_decay(FLAGS.learning_rate,
-                                      global_step,
-                                      decay_steps,
-                                      FLAGS.learning_rate_decay_factor,
-                                      staircase=True,
-                                      name='exponential_decay_learning_rate')
-    
-    
-def get_init_fn():
-    """Returns a function run by che chief worker to warm-start the training.
-    
-    Modified from:
-        https://github.com/tensorflow/models/blob/master/research/slim/
-        train_image_classifier.py
-    
-    Note that the init_fn is only run when initializing the model during the 
-    very first global step.
-    
-    Returns:
-        An init function run by the supervisor.
-    """
-    if FLAGS.checkpoint_path is None:
-        return None
-    
-    # Warn the user if a checkpoint exists in the train_dir. Then we'll be
-    # ignoring the checkpoint anyway.
-    if tf.train.latest_checkpoint(FLAGS.logdir):
-        tf.logging.info(
-            'Ignoring --checkpoint_path because a checkpoint already exists ' +
-            'in %s' % FLAGS.logdir)
-        return None
-    
-    if tf.gfile.IsDirectory(FLAGS.checkpoint_path):
-        checkpoint_path = tf.train.latest_checkpoint(FLAGS.checkpoint_path)
-    else:
-        checkpoint_path = FLAGS.checkpoint_path
-
-    tf.logging.info('Fine-tuning from %s' % checkpoint_path)
-    
-    variables_to_restore = slim.get_variables_to_restore()
-    return slim.assign_from_checkpoint_fn(
-        checkpoint_path,
-        variables_to_restore,
-        ignore_missing_vars=True)
-'''
-
-
-
 
 
 
@@ -169,7 +98,7 @@ def main(_):
     learning_rate = tf.placeholder(tf.float32, name='learning_rate')
     
     # build model correlation op: logits, classed, loss, acc
-    classification_model = resnet_v1_50_model.Model(num_classes=num_classes)
+    classification_model = model.Model(num_classes=num_classes)
 
     #inputs_dict = { 'inputs': inputs,
     #                'is_training': is_training}
@@ -286,11 +215,6 @@ def main(_):
                 print(train_text)
                 sys.stdout.flush()
 
-                #loss_summary.value.add(tag="train_loss", simple_value = loss_)
-                #acc_summary.value.add(tag="train_accuary", simple_value = acc_)
-                #train_writer.add_summary(loss_summary, total_batch_num)
-                #train_writer.add_summary(acc_summary, total_batch_num)
-
             epoch_end_time = time.time()
             print("total use time: {}s\n".format(int(epoch_end_time - epoch_start_time)))
 
@@ -354,43 +278,8 @@ def main(_):
             time.sleep(120) # let gpu take a breath
             print("\n\n")
             sys.stdout.flush()
-
-
-
-    #dataset = get_record_dataset(FLAGS.train_record_path, num_samples=FLAGS.num_samples, 
-    #                             num_classes=FLAGS.num_classes)
-    #data_provider = slim.dataset_data_provider.DatasetDataProvider(dataset)
-    #image, label = data_provider.get(['image', 'label'])
-	# 
-    ## get preprocessed batch data
-    #preprocessed_inputs, labels = preprocessing.preprocess(image, label, is_training=True, batch_size=FLAGS.batch_size)
-    #    
-    #cls_model = model.Model(is_training=True, num_classes=FLAGS.num_classes)
-    #prediction_dict = cls_model.predict(preprocessed_inputs)
-    #loss_dict = cls_model.loss(prediction_dict, labels)
-    #loss = loss_dict['loss']
-    #postprocessed_dict = cls_model.postprocess(prediction_dict)
-    #acc = cls_model.accuracy(postprocessed_dict, labels)
-    #tf.summary.scalar('loss', loss)
-    #tf.summary.scalar('accuracy', acc)
-
-    #global_step = slim.create_global_step()
-    #learning_rate = configure_learning_rate(FLAGS.num_samples, global_step)
-    #optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, 
-    #                                       momentum=0.9)
-#   # optimizer = tf.train.AdamOptimizer(learning_rate=0.00001)
-    #train_op = slim.learning.create_train_op(loss, optimizer,
-    #                                         summarize_gradients=True)
-    #tf.summary.scalar('learning_rate', learning_rate)
-    #
-    #init_fn = get_init_fn()
-    #
-    #sys.stdout.flush()
-    #slim.learning.train(train_op=train_op, logdir=FLAGS.logdir, 
-    #                    init_fn=init_fn, number_of_steps=FLAGS.num_steps,
-    #                    save_summaries_secs=20,
-    #                    save_interval_secs=3600)
     
+
 
 if __name__ == '__main__':
     tf.app.run()
